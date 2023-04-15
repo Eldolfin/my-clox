8f0961e99a8f9ca6e19c2c3dc15305aaba8dcc0c
oscar <90446228+Eldolfin@users.noreply.github.com>
2023-04-15 12:27:05 +0100

    chapter 2: execution and basic arithmetic operations

--- src/main.c
+++ src/main.c
@@ -3,8 +3,10 @@
 #include "chunk.h"
 #include "common.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, char *argv[]) {
+  initVM();
   Chunk chunk;
   initChunk(&chunk);
 
@@ -12,8 +14,25 @@ int main(int argc, char *argv[]) {
   writeChunk(&chunk, OP_CONSTANT, 123);
   writeChunk(&chunk, constant, 123);
 
+  constant = addConstant(&chunk, 3.4);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_ADD, 123);
+
+  constant = addConstant(&chunk, 5.6);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_DIVIDE, 123);
+  for (size_t i = 0; i < 10000; i++) {
+    writeChunk(&chunk, OP_NEGATE, 123);
+  }
+
   writeChunk(&chunk, OP_RETURN, 123);
   disassembleChunk(&chunk, "test chunk");
+  interpret(&chunk);
+  freeVM();
   freeChunk(&chunk);
   return EXIT_SUCCESS;
 }
